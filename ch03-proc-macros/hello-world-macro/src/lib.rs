68bca35d2d076a159ac6390a0bf733e7b431ead0
Collin Van Dyck <collinvandyck@gmail.com>
2024-06-19 08:24:36 -0400

    wip

--- ch03-proc-macros/hello-world-macro/src/lib.rs
+++ ch03-proc-macros/hello-world-macro/src/lib.rs
@@ -1,14 +1,40 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Hello)]
-pub fn hello(_item: TokenStream) -> TokenStream {
+pub fn hello(item: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(item as DeriveInput);
+    let name = ast.ident;
     let res = quote! {
-        impl Example {
+        impl #name {
             fn hello_world(&self) {
                 println!("Hello, World");
             }
         }
     };
     res.into()
+}
+
+#[proc_macro_derive(HelloSimple)]
+pub fn hello_simple(item: TokenStream) -> TokenStream {
+    fn ident_name(item: TokenTree) -> String {
+        match item {
+            TokenTree::Ident(i) => i.to_string(),
+            _ => panic!("no ident"),
+        }
+    }
+    let name = ident_name(item.into_iter().nth(1).unwrap());
+    format!(
+        r#"
+        impl {} {{
+            fn hello_world(&self) {{
+                println!("Hello, World (simple)");
+            }}
+        }}
+        "#,
+        name
+    )
+    .parse()
+    .unwrap()
 }
--- ch03-proc-macros/src/main.rs
+++ ch03-proc-macros/src/main.rs
@@ -1,9 +1,17 @@
-use hello_world_macro::Hello;
+use hello_world_macro::{Hello, HelloSimple};
 
 #[derive(Hello)]
-struct Example;
+struct Target;
+
+#[derive(HelloSimple)]
+enum Pet {
+    Cat,
+}
 
 fn main() {
-    let ex = Example;
+    let ex = Target;
     ex.hello_world();
+
+    let cat = Pet::Cat;
+    cat.hello_world();
 }
